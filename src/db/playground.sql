69d8562c9212de691ac1cc0482349cb138dc4974
Matthew Spriggs <matspriggs@gmail.com>
2025-02-19 19:46:48 +0000

    Refactor playground.sql to comment out unused SQL queries and improve readability

--- src/db/playground.sql
+++ src/db/playground.sql
@@ -76,15 +76,42 @@ RF_001|chr04|6
 -- List genes impacted by moderate or high impact variants in a specific 
 -- chromosome region for a specific VCF {genome_id}
 
-SELECT 
-    variants.genome_id,
-    variants.chromosome_id,
-    variants.info,    
-    variants.snpeff_match
+-- SELECT 
+--     DISTINCT
+--     variants.gene_name,
+--     variants.genome_id,
+--     variants.chromosome_id,   
+--     variants.snpeff_match,
+--     variants.position,
+--     (SELECT start FROM chromosomes WHERE chromosomes.chromosome_id = variants.chromosome_id) AS start,
+--     (SELECT end FROM chromosomes WHERE chromosomes.chromosome_id = variants.chromosome_id) AS end
 
-    
 
-FROM variants
-JOIN chromosomes
-ON variants.chromosome_id = chromosomes.chromosome_id
+-- FROM variants
+-- JOIN chromosomes
+--     ON variants.chromosome_id = chromosomes.chromosome_id
 
+-- WHERE variants.gene_name IS NOT NULL
+-- AND variants.snpeff_match IN ('HIGH')
+-- AND variants.genome_id = 'RF_041'
+-- AND variants.chromosome_id = 'chr03'
+-- AND variants.position > start
+-- AND variants.position < start + (20 * 1000000) -- 20 is user variable
+
+-- LIMIT 5;
+
+/*
+gene_name|genome_id|chromosome_id|snpeff_match
+Solyc03g006480.1.1|RF_041|chr03|HIGH
+Solyc03g033330.2.1|RF_041|chr03|HIGH
+*/
+
+-- List sample which contains a variant that impacts a specific gene
+-- SELECT 
+--     DISTINCT
+--     genomes.genome_id
+-- FROM genomes
+-- JOIN variants
+--     ON genomes.genome_id = variants.genome_id
+
+-- WHERE variants.gene_name = 'Solyc03g006480.1.1'
